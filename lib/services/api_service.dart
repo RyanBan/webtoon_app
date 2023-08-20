3530e1cf46ba09f2072d98bca649159875733441
Ryan Ban <1127ryanban@gmail.com>
2023-08-20 00:39:02 -0400

    Added webtoon detail, episode model, webtoon widget and hero

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:webtoon_app/models/webtoon.dart';
+import 'package:webtoon_app/models/webtoon_detail_model.dart';
+import 'package:webtoon_app/models/webtoon_episode_model.dart';
 
 class ApiService {
   final String baseURL = "https://webtoon-crawler.nomadcoders.workers.dev";
@@ -19,6 +21,30 @@ class ApiService {
       }
       // print(webtoonList);
       return webtoonList;
+    }
+    throw Error();
+  }
+
+  Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseURL/$id");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(String id) async {
+    List<WebtoonEpisodeModel> episodesInstances = [];
+    final url = Uri.parse("$baseURL/$id/episodes");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodesInstances.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodesInstances;
     }
     throw Error();
   }
